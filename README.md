409b59737b139278f15624dbefa262dcd1786383
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2024-09-02 18:02:18 +0200

    Add project skeleton

---
+++ README.md
@@ -0,0 +1,2 @@
+# intentions
+RSpec-inspired BDD-like syntax sugar to force more descriptive test cases and explicit intentions.
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Setup the package.
+"""
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+with open('README.md', 'r', encoding='utf-8') as read_me:
+    long_description = read_me.read()
+
+with open('requirements/project.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+with open('.project-version', 'r') as project_version_file:
+    project_version = project_version_file.read().strip()
+
+setup(
+    version=project_version,
+    name='intentions',
+    description='RSpec-inspired BDD-like syntax sugar to force more descriptive test cases and explicit intentions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dmytrostriletskyi/intentions',
+    project_urls={
+        'Issue Tracker': 'https://github.com/dmytrostriletskyi/intentions/issues',
+        'Source Code': 'https://github.com/dmytrostriletskyi/intentions',
+        'Download': 'https://github.com/dmytrostriletskyi/intentions/tags',
+    },
+    license='MIT',
+    author='Dmytro Striletskyi',
+    author_email='dmytro.striletskyi@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
